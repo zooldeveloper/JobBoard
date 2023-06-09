caca30479d34724dad9f041f43ff6d661384b331
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2023-06-09 17:53:01 +0200

    Initial commit

---
+++ client/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+// import { RouteRecordRaw } from "vue-router"; Used for TypeScript!
+import HomePage from "../views/HomePage.vue";
+
+const routes = [
+	{
+		path: "/",
+		redirect: "/home",
+	},
+	{
+		path: "/home",
+		name: "Home",
+		component: HomePage,
+	},
+];
+
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+});
+
+export default router;
