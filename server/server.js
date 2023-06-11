72ae1a1b30faeeae81ad69fd876bf5867470d937
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2023-06-11 14:12:13 +0200

    Setup server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+
+// require('dotenv').config({});
+
+const data = require('./jobs.json');
+
+const app = express();
+app.use(cors());
+
+app.get('/jobs', (req, res, next) => {
+    try {
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(400).json({ error })
+    }
+});
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
